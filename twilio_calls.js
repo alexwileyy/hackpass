04b452f8f9b49a6cbb01064c58612924567691b2
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2016-10-08 14:16:07 +0100

    multiMessage now working

--- twilio_calls.js
+++ twilio_calls.js
@@ -1,5 +1,6 @@
 var twilio_accountSid = 'AC4b8ab878645d75095b4de7d767226ae8'; // Your Account SID from www.twilio.com/console
 var twilio_authToken = "fd12f91381ef650e6199b3b301f01877";
+var twilio_number = '+441202286170';
 
 var twilio = require('twilio');
 var twilio_client = new twilio.RestClient(twilio_accountSid, twilio_authToken);
@@ -11,7 +12,7 @@ function testMessage(mobileNumber){
     twilio_client.messages.create({
         body: 'Aaron is texting you using node.js from HackUPC :D',
         to: mobileNumber,
-        from: '+441202286170' // From a valid Twilio number
+        from: twilio_number // From a valid Twilio number
     }, function(err, message) {
 
     	if (err) {
@@ -21,3 +22,33 @@ function testMessage(mobileNumber){
         }
     });
 }
+
+function multiMessage(jsonArray, message){
+
+    jsonArray.forEach(function(json){
+        twilio_client.messages.create({
+            body: message,
+            to: json.mobileNumber,
+            from: twilio_number // From a valid Twilio number
+        }, function(err, message) {
+
+            if (err) {
+                console.log(err);
+            }else{
+                console.log(message.sid);
+            }
+        });
+    });
+}
+
+//testMessage(undefined);
+/*
+multiMessage(
+    [
+        { mobileNumber : '+447411305139' },
+        { mobileNumber : '+447904681221' },
+        { mobileNumber : '+447850546917'}
+    ],
+    "Hooray for multiple messages"
+);
+*/
