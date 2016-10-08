350008a9542a7f09deb8495c1c7a1f4475b4fd99
Nat <xdnatty@gmail.com>
2016-10-08 05:23:39 +0100

    Cleaner code, updated dependencies, endpoint work begun
    - Added a secrets.js file - this file contains the API secret as well as exporting it for use in the main app.js
    - Added 'request' requirement - to be used to pull data from the MyMLH API, as well as potentially doing OAuth if it's necessary
    - Major cleaning of code - removed some stuff like if(err) (talk to me about this)
    - Added an endpoint called /create_event and begun major work on it. See comments for more info regarding this
    
    ~ Nat :)

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+// import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const request = require('request');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// import external files
+var secrets = require('./secrets.js'); // import API keys etc
+var appID = secrets.appID;
+var secret = secrets.secret;
+
+// "create_event" route - Bring in the app ID/secret from the Hackathon owner.
+app.post('/create_event', function (req, res) {
+    console.log(req.body);
+    if(req.body.appID && req.body.secret && req.body.eventName){
+      // we have all the stuff we need to pass to OAuth
+      request('http://www.google.com', function (error, response, body) {
+
+        // !!!! IMPORTANT
+
+        // This snippet here currently stops the app sending a response.
+
+        // Try sending a POST with invalid values, you should get a valid response
+        // Now try sending a POST with valid stuff, Postman will tell you "Could not get any response"
+        // It was at this point I decided to sleep, but this should be decent progress
+        // Next step is to call:
+
+        // GET https://my.mlh.io/api/v2/users.json?client_id=<CLIENT_ID>&secret=<SECRET>&page=1
+
+        // to pull the data for our event. This works in Postman, but the functionality is currently broken in the app.
+
+        // Good luck <3 - Nat
+
+        if (!error && response.statusCode == 200) {
+          req.send(body) // Show the HTML for the Google homepage.
+        }
+      })
+    }
+    else{
+      res.status(400).send('Bad Request');
+    }
+})
+
 
 //Standard entry point.
-app.get('/', function(req, res, err){
-    if(err){
-      console.log(err)
-    }
+app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
+
 
 app.listen(9001, function(){
     console.log('App running on 9001');
---
+++ secrets.js
@@ -0,0 +1,7 @@
+//appid
+var appID = '3b0f0c7b101653f2ba1cdd2aa2ed2c84a38f50875cd6fdb56c535e894b2e9b3a';
+exports.appID = appID;
+
+//secret
+var secret = 'f32f5835e2df0a71e2018aeceb8d9131637db294b04d9404b113129c311a57a2';
+exports.secret = secret;
