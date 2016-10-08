efed970fe8f3ae3f2e53502239db05f9850ae70b
Alex <alexwileyy@outlook.com>
2016-10-08 03:07:04 +0200

    Uploaded mongoose

--- db.js
+++ db.js
@@ -1,4 +1,22 @@
 var mongoose = require('mongoose');
-
 mongoose.connect('localhost:27017');
 
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    user: String,
+    email: String
+});
+
+var userTable = mongoose.model('User', UserSchema);
+
+
+function addUser()
+var newUser = new userTable({ user: 'Zildjian', email: 'alex' });
+
+newUser.save(function(err){
+   if(err){
+       console.log(err)
+   }
+   console.log('yay')
+});
