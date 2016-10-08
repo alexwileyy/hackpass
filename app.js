01006e5d405d9dcd5560186992998c82eb6b6e07
Alex <alexwileyy@outlook.com>
2016-10-08 20:23:04 +0200

    Created organiser panel

--- app.js
+++ app.js
@@ -18,9 +18,33 @@ var secret = secrets.secret;
 //Share the public folder.
 app.use(express.static('public'));
 
+//Standard entry point.
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+//Follows the home page when a valid APP ID and SECRET code have been entered.
+app.get('/event', function(req, res){
+    res.sendFile(path.join(__dirname + '/create-event.html'), (err) =>{
+        if(err){
+            console.log(err)
+        }
+    })
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile(__dirname + '/organiser.html', (err) => {
+        if(err) console.log(err)
+    });
+});
+
+app.get('/profile/:id', (req, res) => {
+    //Mongo to fetch single user.
+});
+
 
 // "create_event" route - Bring in the app ID/secret from the Hackathon owner.
-app.post('/create_event', function (req, res) {
+app.post('/create_event', (req, res) => {
     if(req.body.appID && req.body.secret){
         //Obtain the AppID and Secret from page
         const appID = req.body.appID;
@@ -39,7 +63,7 @@ app.post('/create_event', function (req, res) {
             const data = JSON.parse(body);
             //Call addUsers mongo
             db.addEvent(data, function(event){
-                const id = JSON.stringify(event._id);
+                const id = String(event._id);
                 res.redirect('event/?event_id=' + id);
             });
         });
@@ -50,21 +74,6 @@ app.post('/create_event', function (req, res) {
         console.log('bad request');
     }
 });
-
-app.get('/event', function(req, res){
-   res.sendFile(path.join(__dirname + '/create-event.html'), (err) =>{
-       if(err){
-           console.log(err)
-       }
-   })
-});
-
-//Standard entry point.
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-
 
 app.listen(9001, function(){
     console.log('App running on 9001');
