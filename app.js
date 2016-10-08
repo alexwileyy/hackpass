cd929e8da6a9bb2e38f3695c62ae3184c0df2d95
Alex <alexwileyy@outlook.com>
2016-10-08 02:14:59 +0200

    Configured express

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 //Standard entry point.
 app.get('/', function(req, res, err){
     if(err){
-      console.log(err);
+      console.log(err)
     }
-    res.send('Hello')
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(8000, function(){
-    console.log('User Connected')
+app.listen(9001, function(){
+    console.log('App running on 9001');
+    console.log('User Connected');
 });
