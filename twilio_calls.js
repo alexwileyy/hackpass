05fa9a87afab81926e0683299c9c3a5937cd9890
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2016-10-08 13:08:58 +0100

    Fixed forgetting to rename parameters

--- twilio_calls.js
+++ twilio_calls.js
@@ -2,13 +2,13 @@ var twilio_accountSid = 'AC4b8ab878645d75095b4de7d767226ae8'; // Your Account SI
 var twilio_authToken = "fd12f91381ef650e6199b3b301f01877";
 
 var twilio = require('twilio');
-var twilio_client = new twilio.RestClient(accountSid, authToken);
+var twilio_client = new twilio.RestClient(twilio_accountSid, twilio_authToken);
 
 
 function testMessage(mobileNumber){
     if (mobileNumber == undefined) mobileNumber = '+447850546917';
 
-    client.messages.create({
+    twilio_client.messages.create({
         body: 'Aaron is texting you using node.js from HackUPC :D',
         to: mobileNumber,
         from: '+441202286170' // From a valid Twilio number
